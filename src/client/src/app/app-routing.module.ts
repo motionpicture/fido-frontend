2b0565adc37915f22579c64cde616bf693081a2f
Akito Hataguchi <hataguchi@motionpicture.jp>
2018-08-21 16:30:47 +0900

    検証用

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -3,29 +3,14 @@
  */
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './components/pages/error/error.component';
-import { NotFoundComponent } from './components/pages/not-found/not-found.component';
-import { TutorialComponent } from './components/pages/tutorial/tutorial.component';
-import * as auth from './routes/auth.route';
-import * as common from './routes/common.route';
+import { IndexComponent } from './components/pages/index/index.component';
 import * as fido from './routes/fido.route';
-import * as member from './routes/member.route';
-import * as purchase from './routes/purchase.route';
-import * as register from './routes/register.route';
+import { FidoGuardService } from './services/fido-guard/fido-guard.service';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/member/mypage', pathMatch: 'full' },
-    common.route,
-    auth.route,
-    auth.logoutRoute,
-    register.route,
-    member.route,
-    fido.route,
-    purchase.route,
-    { path: 'tutorial', component: TutorialComponent },
-    { path: 'error/:redirect', component: ErrorComponent },
-    { path: 'error', component: ErrorComponent },
-    { path: '**', component: NotFoundComponent }
+    { path: '', redirectTo: '/', pathMatch: 'full' },
+    { path: '', canActivate: [FidoGuardService], component: IndexComponent },
+    fido.route
 ];
 
 // tslint:disable-next-line:no-stateless-class
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -10,48 +10,10 @@ import { MomentModule } from 'angular2-moment';
 import { SwiperModule } from 'ngx-swiper-wrapper';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
-import { AboutComponent } from './components/pages/about/about.component';
-import { AuthLogoutComponent } from './components/pages/auth/auth-logout/auth-logout.component';
-import { AuthSelectComponent } from './components/pages/auth/auth-select/auth-select.component';
-import { AuthSigninComponent } from './components/pages/auth/auth-signin/auth-signin.component';
-import { AuthSignoutComponent } from './components/pages/auth/auth-signout/auth-signout.component';
 import { BaseComponent } from './components/pages/base/base.component';
-import { BenefitsComponent } from './components/pages/benefits/benefits.component';
-import { ErrorComponent } from './components/pages/error/error.component';
-import { ExpiredComponent } from './components/pages/expired/expired.component';
 import { FidoRegisterComponent } from './components/pages/fido/fido-register/fido-register.component';
 import { FidoRemoveComponent } from './components/pages/fido/fido-remove/fido-remove.component';
-import { LawComponent } from './components/pages/law/law.component';
-import { MemberBenefitsComponent } from './components/pages/member/member-benefits/member-benefits.component';
-import { MemberEditCreditComponent } from './components/pages/member/member-edit-credit/member-edit-credit.component';
-import { MemberEditProfileComponent } from './components/pages/member/member-edit-profile/member-edit-profile.component';
-import { MemberEditComponent } from './components/pages/member/member-edit/member-edit.component';
-import { MemberMypageComponent } from './components/pages/member/member-mypage/member-mypage.component';
-import { MemberPointHistoryComponent } from './components/pages/member/member-point-history/member-point-history.component';
-import { MemberPointComponent } from './components/pages/member/member-point/member-point.component';
-import { MemberWithdrawComponent } from './components/pages/member/member-withdraw/member-withdraw.component';
-import { NotFoundComponent } from './components/pages/not-found/not-found.component';
-import { PolicyComponent } from './components/pages/policy/policy.component';
-import { PrivacyComponent } from './components/pages/privacy/privacy.component';
-import { PurchaseBaseComponent } from './components/pages/purchase/purchase-base/purchase-base.component';
-import { PurchaseCompleteComponent } from './components/pages/purchase/purchase-complete/purchase-complete.component';
-import { PurchaseConfirmComponent } from './components/pages/purchase/purchase-confirm/purchase-confirm.component';
-import { PurchaseInputComponent } from './components/pages/purchase/purchase-input/purchase-input.component';
-import { PurchaseScheduleComponent } from './components/pages/purchase/purchase-schedule/purchase-schedule.component';
-import { PurchaseSeatComponent } from './components/pages/purchase/purchase-seat/purchase-seat.component';
-import { PurchaseTicketComponent } from './components/pages/purchase/purchase-ticket/purchase-ticket.component';
-import { PurchaseTransactionComponent } from './components/pages/purchase/purchase-transaction/purchase-transaction.component';
-import { QrcodeCompleteComponent } from './components/pages/qrcode/qrcode-complete/qrcode-complete.component';
-import { QrcodeConfirmComponent } from './components/pages/qrcode/qrcode-confirm/qrcode-confirm.component';
-import { QrcodeIndexComponent } from './components/pages/qrcode/qrcode-index/qrcode-index.component';
-import { RegisterCreditComponent } from './components/pages/register/register-credit/register-credit.component';
-import { RegisterComponent } from './components/pages/register/register-index/register-index.component';
-import {
-    RegisterProgramMembershipComponent
-} from './components/pages/register/register-program-membership/register-program-membership.component';
-import { RegisterTermsComponent } from './components/pages/register/register-terms/register-terms.component';
-import { TicketComponent } from './components/pages/ticket/ticket.component';
-import { TutorialComponent } from './components/pages/tutorial/tutorial.component';
+import { IndexComponent } from './components/pages/index/index.component';
 import { ButtonsComponent } from './components/parts/buttons/buttons.component';
 import { HeaderMenuComponent } from './components/parts/header-menu/header-menu.component';
 import { HeaderComponent } from './components/parts/header/header.component';
@@ -82,73 +44,36 @@ import { TimeFormatPipe } from './pipes/time-format/time-format.pipe';
 @NgModule({
     declarations: [
         AppComponent,
-        TutorialComponent,
         NavigationComponent,
         HeaderComponent,
-        NotFoundComponent,
         BaseComponent,
         TimeFormatPipe,
         LibphonenumberFormatPipe,
         AvailabilityPipe,
         DurationPipe,
-        AboutComponent,
         LoadingComponent,
-        PolicyComponent,
-        LawComponent,
-        PrivacyComponent,
-        ErrorComponent,
         TicketDetailComponent,
         TicketNotFoundComponent,
         HeaderMenuComponent,
-        TicketComponent,
-        AuthSelectComponent,
-        AuthLogoutComponent,
-        MemberBenefitsComponent,
-        MemberEditComponent,
-        MemberWithdrawComponent,
-        MemberPointComponent,
-        MemberPointHistoryComponent,
-        MemberMypageComponent,
         PurchasePerformanceTimeComponent,
         PurchasePerformanceFilmComponent,
         IconComponent,
-        AuthSigninComponent,
-        AuthSignoutComponent,
-        MemberEditCreditComponent,
         ModalComponent,
-        MemberEditProfileComponent,
-        RegisterTermsComponent,
-        RegisterComponent,
         MembershipBenefitsComponent,
         PointSliderComponent,
         PointStampsComponent,
-        BenefitsComponent,
         DateFormatPipe,
-        RegisterCreditComponent,
-        RegisterProgramMembershipComponent,
         InformationComponent,
         PageComponent,
         PointHistoryListComponent,
         MaintenanceComponent,
-        PurchaseScheduleComponent,
         PurchaseStepComponent,
-        PurchaseBaseComponent,
-        PurchaseCompleteComponent,
-        PurchaseScheduleComponent,
-        PurchaseSeatComponent,
-        PurchaseTicketComponent,
-        PurchaseInputComponent,
-        PurchaseConfirmComponent,
-        PurchaseTransactionComponent,
         ScreenComponent,
         SeatInfoComponent,
         ButtonsComponent,
-        ExpiredComponent,
         FidoRegisterComponent,
         FidoRemoveComponent,
-        QrcodeIndexComponent,
-        QrcodeConfirmComponent,
-        QrcodeCompleteComponent
+        IndexComponent
     ],
     imports: [
         BrowserModule,
